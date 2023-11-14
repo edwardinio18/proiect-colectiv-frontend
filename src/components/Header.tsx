238466a0878fbb623c7c21d518c9441165a4b086
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2023-11-14 20:09:49 +0200

    Gamemodes

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/Header.css';
+
+export const Header: React.FC = () => {
+  return (
+    <header className="game-header">
+      <div className="header-title">MioriticMinds</div>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Rankings</a></li>
+          <li><a href="#">My Account</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
