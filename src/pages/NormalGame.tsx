4e8837a33bc85763d1ce7f25edee1d465eb29e75
Cristina Hognogi <90508610+hognogicristina@users.noreply.github.com>
2023-11-18 18:27:11 +0200

    go back button

--- src/pages/NormalGame.tsx
+++ src/pages/NormalGame.tsx
@@ -1,6 +1,8 @@
 import '../styles/Mode.css';
 import '../styles/Game.css';
 import Header from '../components/Header';
+import {Link} from "react-router-dom";
+import React from "react";
 
 export const NormalGame: React.FC = () => {
     return (
@@ -8,6 +10,9 @@ export const NormalGame: React.FC = () => {
             <Header/>
             <div className="game-main">
                 <h1>Not available yet</h1>
+                <div className="game-mode-buttons">
+                    <Link to="/gamemode" className="game-mode-button">Go back</Link>
+                </div>
             </div>
         </div>
     );
