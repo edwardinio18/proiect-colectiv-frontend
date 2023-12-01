1d2927fcd821fd954ac06f44d4425c4a93ac2695
Andrei Albert <afkck55@gmail.com>
2023-12-01 17:09:16 +0200

    added support for photos

--- src/pages/PracticeGame.tsx
+++ src/pages/PracticeGame.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { Question, Answer } from '../interfaces/Question';
 import { shufleArray } from '../utils/shuffleArr';
+import { Button } from '@mui/material';
 
 
 const BASE_API_URL = import.meta.env.VITE_API_URL;
@@ -15,13 +16,17 @@ export const PracticeGame: React.FC = () => {
     const [answers, setAnswers] = useState<Answer[]>([]);
     const [selectedAnswer, setSelectedAnswer] = useState<Answer | null>(null);
     const [hasAnswered, setHasAnswered] = useState<boolean>(false);
+    const [imageUrl, setImageUrl] = useState<string>('');
 
     const fetchQuestions = async (): Promise<void> => {
         setIsLoading(true);
         setSelectedAnswer(null);
         setAnswers([]);
-       //const randomEndpoint = QUESTION_ENDPOINTS[Math.floor(Math.random() * QUESTION_ENDPOINTS.length)];
-       const randomEndpoint = QUESTION_ENDPOINTS[0];
+        setImageUrl('');
+        setHasAnswered(false);
+        const randomPosition = Math.floor(Math.random() * QUESTION_ENDPOINTS.length);
+        console.log(`random pos is ${randomPosition}`)
+        const randomEndpoint = QUESTION_ENDPOINTS[randomPosition];
         try {
             const response = await fetch(randomEndpoint, {
                 method: 'POST'
@@ -31,6 +36,9 @@ export const PracticeGame: React.FC = () => {
             }
             const data: Question = await response.json();
             const allAnswers: Answer[] = [];
+            if(randomPosition === 1) {
+                setImageUrl(data.summary);
+            }
             allAnswers.push({
                 answer: data.name, 
                 isCorrect: true
@@ -42,10 +50,8 @@ export const PracticeGame: React.FC = () => {
                 })
             })
             setCurrentQuestion(data);
-            setAnswers(allAnswers)
             shufleArray(allAnswers);
-            console.log(allAnswers);
-            console.log(data);
+            setAnswers(allAnswers)
         } catch(error) {
             console.log(error);
         } finally {
@@ -65,11 +71,11 @@ export const PracticeGame: React.FC = () => {
         }
         else {
             console.log('no');
-            return;
+            
         }
         setTimeout(() => {
             fetchQuestions();
-        }, 1000);
+        }, 2000);
     }
 
     if(isLoading) {
@@ -78,15 +84,27 @@ export const PracticeGame: React.FC = () => {
 
 
     const renderAnswers = (answers: Answer[]) => {
-        answers = shufleArray(answers);
-        return answers.map((answer, index) => (
-            <button
-                key={index}
-                onClick={() => handleAnswer(answer)}
-            >
-                {answer.answer}
-            </button>
-        ));
+        return answers.map((answer, index) => {
+            let style = {}; 
+            if(hasAnswered) {
+                if(answer.isCorrect) {
+                    style = {backgroundColor: 'green', color: 'white'}; 
+                }else {
+                    style=  {backgroundColor: 'red', color: 'white'}; 
+                }
+            }
+            return (
+                <Button 
+                    key={index}
+                    onClick={() => handleAnswer(answer)}
+                    style={style}
+                    variant='contained'
+                    disabled={hasAnswered}
+                >
+                    {answer.answer}
+                </Button>
+            )
+        });
     };
     
 
@@ -94,7 +112,8 @@ export const PracticeGame: React.FC = () => {
         <div>
             {currentQuestion && (
                 <>
-                    <h2>{currentQuestion.summary}</h2>
+                    {imageUrl && <img src={imageUrl} alt={currentQuestion.name}/>}
+                    {!imageUrl && <h2>{currentQuestion.summary}</h2>}
                     <div>{renderAnswers(answers)}</div>
                 </>
             )}
