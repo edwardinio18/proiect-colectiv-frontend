0f06b3d54e63d9d2cb9235b63a1b5c2d191fbe12
Edward Iakab <edwardiakab@yahoo.com>
2024-01-11 19:11:50 +0200

    Add user account management.

--- src/pages/MyAccount.tsx
+++ src/pages/MyAccount.tsx
@@ -7,53 +7,69 @@ import '../styles/GameModes.css';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-export interface User {
-    username: string;
-    totalScore: number;
+const getUserUsername = async (userUsernameUrl: string) => {
+  const response = await fetch(userUsernameUrl);
+  return await response.text();
+};
+
+const getMixedUserScore = async (userUrl: string) => {
+  const response = await fetch(userUrl);
+  return await response.json();
 }
 
-const getUserUsername = async (userUsernameUrl: string) => {
-    const response = await fetch(userUsernameUrl);
-    return await response.text();
-};
+const getTextUserScore = async (userUrl: string) => {
+  const response = await fetch(userUrl);
+  return await response.json();
+}
 
-const getTotalScore = async (userScoreUrl: string) => {
-    const response = await fetch(userScoreUrl);
-    return await response.json();
-};
+const getImageUserScore = async (userUrl: string) => {
+  const response = await fetch(userUrl);
+  return await response.json();
+}
 
 export const MyAccount: React.FC = () => {
-    const TOKEN = jwtDecode<Jwt>(localStorage.getItem('token')!);
-    const USER_ID = TOKEN.nameid;
-    const USER_USERNAME_URL = `${API_URL}/Users/${USER_ID}`;
-    const USER_SCORE_URL = `${API_URL}/Users/${USER_ID}/score`;
+  const TOKEN = jwtDecode<Jwt>(localStorage.getItem('token')!);
+  const USER_ID = TOKEN.nameid;
+  const USER_USERNAME_URL = `${API_URL}/Users/${USER_ID}`;
 
-    const [userUsername, setUserUsername] = useState('');
-    const [totalScore, setTotalScore] = useState<number | null>(null);
+  const LEADERBOARD_URL = `${API_URL}/Leaderboard`;
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const userUsernameData = await getUserUsername(USER_USERNAME_URL);
-            setUserUsername(userUsernameData);
+  const [userUsername, setUserUsername] = useState('');
+  const [userMixedScore, setUserMixedScore] = useState<number>(0);
+  const [userTextScore, setUserTextScore] = useState<number>(0);
+  const [userImageScore, setUserImageScore] = useState<number>(0);
 
-            const userScoreData = await getTotalScore(USER_SCORE_URL);
-            setTotalScore(userScoreData.totalScore);
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const userUsernameData = await getUserUsername(USER_USERNAME_URL);
+      setUserUsername(userUsernameData);
 
-        fetchData();
-    }, [USER_USERNAME_URL, USER_SCORE_URL]);
+      const userMixedScoreData = await getMixedUserScore(`${LEADERBOARD_URL}/mixed/${userUsernameData}`);
+      setUserMixedScore(userMixedScoreData.score);
 
-    return (
-        <div className="game-container">
-            <Header/>
-            <div className="game-main">
-                <h1>Username: {userUsername}</h1>
-                <p>Total Score: {totalScore !== null ? totalScore : 'Loading...'}</p>
-                <Link to="/gamemode" className="game-mode-button">Game Modes</Link>
-                <Link to="/editmyaccount" className="game-mode-button">Edit Account</Link>
-            </div>
-        </div>
-    );
+      const userTextScoreData = await getTextUserScore(`${LEADERBOARD_URL}/text/${userUsernameData}`);
+      setUserTextScore(userTextScoreData.score);
+
+      const userImageScoreData = await getImageUserScore(`${LEADERBOARD_URL}/photos/${userUsernameData}`);
+      setUserImageScore(userImageScoreData.score);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="game-container">
+      <Header/>
+      <div className="game-main">
+        <h1>Username: {userUsername}</h1>
+        <h2>Mixed Score: {userMixedScore}</h2>
+        <h2>Text Score: {userTextScore}</h2>
+        <h2>Image Score: {userImageScore}</h2>
+        <Link to="/gamemode" className="game-mode-button">Game Modes</Link>
+        <Link to="/editmyaccount" className="game-mode-button">Edit Account</Link>
+      </div>
+    </div>
+  );
 };
 
 export default MyAccount;
